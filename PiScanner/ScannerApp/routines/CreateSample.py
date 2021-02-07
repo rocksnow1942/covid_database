2db42aec5a5d0ffd2b8b101bef363b77435af543
rocksnow1942 <rocksnow1942@gmail.com>
2021-02-07 14:29:56 -0800

    ON: Sun Feb  7 14:29:45 2021, FROM: Hui-PC

--- PiScanner/ScannerApp/routines/CreateSample.py
+++ PiScanner/ScannerApp/routines/CreateSample.py
@@ -4,9 +4,9 @@ from datetime import datetime
 
 class CreateSample(Routine):
     """Scan a rack of samples and store the valid sample tube IDs to database."""
-    _pages = ['DTMXPage','SavePage']
-    _titles = ['Place Plate on reader','Save Sample IDs to database']
-    _msgs = ['Click Read to scan sample IDs','Review the results and click Save']
+    _pages = ['BarcodePage','DTMXPage','SavePage']
+    _titles = ['Scan Reception Barcode','Place Plate on reader','Save Sample IDs to database']
+    _msgs = ["Scan Reception Barcode",'Click Read to scan sample IDs','Review the results and click Save']
     btnName = 'Create'
     requireAuthorization = 'reception'
     @property
@@ -14,33 +14,38 @@ class CreateSample(Routine):
         "return totoal exptected sample count"        
         return 96
     
-    def validateResult(self, wells):
-        self.toUploadSamples = []
-        self.toUpdateSamples = []
-        validlist = [self.master.validate(id,'sample') for (wn,id) in wells]
-        res = self.master.db.get('/samples',json={'sampleId':{'$in':[i[1] for i in wells]}})
-        if res.status_code == 200:
-            conflictSample = [] # samples that is not created by batchDownload, this will mean the sample is already preexist in database.
-            validexist = [] # sample created by batchDownlaod 
-            for s in res.json():
-                if s.get('meta',{}).get('from',None) == 'appCreated' and (not s.get('sPlate',None)):
-                    validexist.append(s.get('sampleId'))
-                    
-                else:
-                    conflictSample.append(s.get('sampleId'))
-            for idx , (wn,id) in enumerate(wells):
-                if id in conflictSample:
-                    validlist[idx] = False
-                  
-            msg = f'{sum(validlist)} / {len(validlist)} valid sample IDs found. \n\
-{len(validexist)}  / {len(validlist)} sampleIDs are downloaded from app\n\
-{len(conflictSample)} / {len(validlist)} samples have conflict with existing sampleIDs'              
-        
-            self.toUploadSamples = [i for i,v in zip(wells,validlist) if (v and (i[1] not in validexist))]
-            self.toUpdateSamples = [i for i,v in zip(wells,validlist) if (v and (i[1] in validexist))]
-            return validlist, msg ,len(conflictSample)==0
+    def validateResult(self, result):
+        if self.currentPage==0:
+            print(result)
+
+            return True,'Batch Reception Barcode Scanned',True
         else:
-            return [False]*len(wells),f'Server Response {res.status_code}',False
+            wells=result
+            self.toUploadSamples = []
+            self.toUpdateSamples = []
+            validlist = [self.master.validate(id,'sample') for (wn,id) in wells]
+            res = self.master.db.get('/samples',json={'sampleId':{'$in':[i[1] for i in wells]}})
+            if res.status_code == 200:
+                conflictSample = [] # samples that is not created by batchDownload, this will mean the sample is already preexist in database.
+                validexist = [] # sample created by batchDownlaod 
+                for s in res.json():
+                    if s.get('meta',{}).get('from',None) == 'appCreated' and (not s.get('sPlate',None)):
+                        validexist.append(s.get('sampleId'))                    
+                    else:
+                        conflictSample.append(s.get('sampleId'))
+                for idx , (wn,id) in enumerate(wells):
+                    if id in conflictSample:
+                        validlist[idx] = False
+                    
+                msg = f'{sum(validlist)} / {len(validlist)} valid sample IDs found. \n\
+    {len(validexist)}  / {len(validlist)} sampleIDs are downloaded from app\n\
+    {len(conflictSample)} / {len(validlist)} samples have conflict with existing sampleIDs'              
+            
+                self.toUploadSamples = [i for i,v in zip(wells,validlist) if (v and (i[1] not in validexist))]
+                self.toUpdateSamples = [i for i,v in zip(wells,validlist) if (v and (i[1] in validexist))]
+                return validlist, msg ,len(conflictSample)==0
+            else:
+                return [False]*len(wells),f'Server Response {res.status_code}',False
         
         
     
@@ -48,10 +53,11 @@ class CreateSample(Routine):
         return f"Total Valid Sample IDs: {len(self.toUploadSamples)}."
 
     def saveResult(self):
-        
+        receptionCode = self.results[0]
+        meta = {"receptionCode":receptionCode}
         # valid = self.validatedWells
-        valid = [{'sampleId':id,'receivedAt':datetime.now().isoformat(),'sWell':wn} for (wn,id) in self.toUploadSamples]
-        update = [{'sampleId':id,'receivedAt':datetime.now().isoformat(),'sWell':wn} for (wn,id) in self.toUpdateSamples]
+        valid = [{'sampleId':id,'receivedAt':datetime.now().isoformat(),'sWell':wn,'meta':meta} for (wn,id) in self.toUploadSamples]
+        update = [{'sampleId':id,'receivedAt':datetime.now().isoformat(),'sWell':wn,'meta.receptionCode':receptionCode} for (wn,id) in self.toUpdateSamples]
         yield f'Saving {len(valid)} samples to database...'
         
         if valid:
--- models/Batch.js
+++ models/Batch.js
@@ -3,30 +3,22 @@ const mongoose = require('mongoose')
 
 // schema for Order data. 
 const Batch = new mongoose.Schema({
-    docID:{ // the accesion log docID, /collectionName/documentID
+    batchId:{ // the accesion log docID, /collectionName/documentID
         type:String,
         trim:true,
-        required:[true,'order ID requried'],
+        required:[true,'batch ID requried'],
         unique:true,
         index:true
     }, // order number. can be used externally
-    patientIds:[{type:String,trim:true}], // a list of patient ID in this batch.
-    sampleIds:[{type:String,trim:true}], // a list of sample IDs in this batch.
     
     createdAt:{type:Date,default:Date.now}, // batch create time,
+    
     sampleCount: Number, //number of samples submitted
+
     receivedCount:Number, //number of samples received,
-    received:Boolean, // whether this batch is received,
-    tested:Boolean, // whether test is done,
-    downloaded:Boolean, // whether user downloaded data,
+   
     note:String, // user note or file name
-    contact: {
-        name: String,
-        tel: String,
-        email: String,
-    }, // contact information
-    // meta  contain fields after reception.
-    // sampleIds missing, sampleIds received, sample Ids unsatisfactory, sampleIds aberrant, 
+  
     meta: {},// orther information
 },)
  
