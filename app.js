cb191e6a7e7e3b89528e2511fe3a6ed9d1063046
rocksnow1942 <rocksnow1942@gmail.com>
2021-03-02 22:46:06 -0800

    ON: Tue Mar  2 22:46:06 2021, FROM: Huis-Mac-Pro.local

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require('dotenv/config')
 const logger = require('./utils/logger')
-// const cors = require('cors')
+const cors = require('cors')
 const express = require('express')
 const app = express()
 
