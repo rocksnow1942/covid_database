493254cb95f6dbf9e22f143af57718732a2370a3
rocksnow1942 <rocksnow1942@gmail.com>
2021-03-02 22:34:45 -0800

    ON: Tue Mar  2 22:34:44 2021, FROM: Huis-Mac-Pro.local

--- routes/plateRoute.js
+++ routes/plateRoute.js
@@ -57,7 +57,7 @@ return json list of plates.
 router.post("/query", (req, res) => {
   Plate.find(req.body, null, { lean: true })
     .sort({ created: 1 })    
-    .then((docs) => res.header('Access-Control-Allow-Origin','*').json(docs))
+    .then((docs) => res.header('Access-Control-Allow-Origin','http://127.0.0.1:5000').json(docs))
     .catch((err) => ErrorHandler(err, res));
 });
 
